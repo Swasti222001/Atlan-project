6693f4264e191430ba7d7bee4b4cc59f41be1c34
Swasti <swastiofficial2001@gmail.com>
2022-06-03 00:11:36 +0530

    buttom comp made

---
+++ src/component/buttoncomp/button.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./button.module.css";
+
+const ButtonCustom = ({
+  btnText,
+  primary,
+  secondary,
+  leftIcon,
+  rightIcon,
+  customStyle,
+  onClick,
+}) => {
+  return (
+    <button
+      className={
+        styles.buttonClass +
+        ` ${
+          primary ? styles.primaryBtn : secondary ? styles.secondaryBtn : " "
+        }`
+      }
+      style={customStyle}
+      onClick={onClick}
+    >
+      {leftIcon}
+      <span className={styles.text}>{btnText}</span>
+      {rightIcon}
+    </button>
+  );
+};
+
+export default ButtonCustom;
--- src/layouts/mainlayout.js
+++ src/layouts/mainlayout.js
@@ -3,6 +3,10 @@ import Listlayout from "../component/listcomp/list";
 import Header from "../component/headercomp/header";
 import InputFields from "../component/Inputcomp/input";
 import styles from './mainlayout.module.css'
+import ButtonCustom from "../component/buttoncomp/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
+
 const mainlayout = () => {
   return (
     <div className={styles.mainlayoutdiv}>
@@ -17,6 +21,13 @@ const mainlayout = () => {
       <InputFields />
       </div>
       </div>
+      <ButtonCustom
+          secondary
+          customStyle={{ marginTop: '1rem' }}
+          btnText={'Run Query'}
+          // onClick={handleClick}
+          rightIcon={<FontAwesomeIcon icon={faCaretRight} />}
+        />
     </div>
   );
 };
